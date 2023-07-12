c8198bc1ef00f81b62992a95247e114ddc654729
Jacob <jacobphelan@hotmail.com>
2023-07-12 16:51:03 +1000

    Add Save and Load through YAML

--- brain.rb
+++ brain.rb
@@ -1,13 +1,17 @@
 class Brain
-  attr_accessor :start_state, :word
-  def initialize
-    self.start_state = gets.chomp
-    word_list = File.open("word_list.txt")
-    @easy_list = []
-    @normal_list = []
-    @hard_list = []
-    self.word = ''
-    load_dictionary(word_list)
+  attr_accessor :start_state, :word, :load
+  def initialize(load=false)
+		self.load = load
+		if self.load == false	
+		  @start_states = ['new game', 'new', 'load']
+    	self.start_state = gets.chomp until @start_states.include?(self.start_state)
+    	word_list = File.open("word_list.txt")
+    	@easy_list = []
+    	@normal_list = []
+    	@hard_list = []
+    	self.word = ''
+    	load_dictionary(word_list)
+		end
   end
 
   def load_dictionary(word_list)
@@ -27,6 +31,10 @@ class Brain
     self.word = @easy_list.sample.upcase if difficulty == 'easy'
     self.word = @normal_list.sample.upcase if difficulty == 'normal'
     self.word = @hard_list.sample.upcase if difficulty == 'hard'
+		#close dictionaries for faster saving
+		@easy_list = []
+		@normal_list = []
+		@hard_list = []
   end
 
   def check_guess(guess)
--- game.rb
+++ game.rb
@@ -6,43 +6,75 @@ We can start new games or load existing ones
 require_relative 'brain'
 require_relative 'display'
 
-class Game
-attr_accessor :game_end
+require 'yaml'
 
-  def initialize
-    @display = DisplayGame.new
-    @brain = Brain.new
-    @difficulties = ['easy', 'normal', 'hard']
-    @difficulty = ""
-		@valid_guesses = /[A-z]/
-    new_game if @brain.start_state.to_s.downcase == 'new game'
-    load_game if @brain.start_state.downcase == 'load'
-  end
+class Game
+  def initialize(display=DisplayGame.new, brain=Brain.new, difficulties=['easy', 'normal', 'hard'], difficulty="", valid_guesses=/[A-z]/, load=false)
+		@load = load
+		@display = display
+		@brain = brain
+		@difficulties = difficulties
+		@difficulty = difficulty
+		@valid_guesses = valid_guesses
+    if @load == false
+			new_game if @brain.start_state.to_s.downcase == 'new game' || @brain.start_state.to_s.downcase == 'new'
+    	load_game("save.yaml") if @brain.start_state.to_s.downcase == 'load'
+		elsif @load == true
+			continue_game
+		end
+	end
 
   def new_game
     pick_difficulty
     @brain.get_secret_word(@difficulty)
     @display.setup_reveal_word(@brain.word)
     turn until @display.winner == true || @display.mistake_counter == 7
-		system 'clear'
-		puts @display.screen
-		@brain.resolve(@display.winner)
+    system 'clear'
+    puts @display.screen
+    @brain.resolve(@display.winner)
   end
 
-  def load_game
-    puts 'chyeah'
-  end
-
-	def turn
-		puts @display.screen
-		puts "What letter do you guess?"
-		letter = gets.chomp.upcase until @valid_guesses.match?(letter)
-		system 'clear'
-		@brain.check_guess(letter)? @display.correct(letter, @brain.word) : @display.error_step(letter)
+  def load_game(string)
+		data = YAML.load(File.read(string), permitted_classes: [DisplayGame, Symbol, Brain, Regexp])
+		p data
+		self.initialize(data[:display], data[:brain], data[:difficulties], data[:difficulty], data[:valid_guesses], true)
 	end
 
+	def continue_game
+		turn until @display.winner == true || @display.mistake_counter == 7
+    system 'clear'
+    puts @display.screen
+    @brain.resolve(@display.winner)
+	end
+
+  def save_game
+		@brain.start_state = 'load'
+		@brain.load = false
+		save_object = YAML.dump ({
+			:display => @display,
+			:brain => @brain,
+      :difficulties => @difficulties,
+      :difficulty => @difficulty,
+      :valid_guesses => @valid_guesses
+		})
+		File.open("save.yaml", "w") { |file| file.write(save_object)}
+
+	end
+
+  def turn
+    puts @display.screen
+    puts "What letter do you guess? Additionally, type \'save\' to save."
+    letter = gets.chomp.upcase until @valid_guesses.match?(letter)
+		if letter == 'SAVE' 
+			save_game
+	  else
+      system 'clear'
+      @brain.check_guess(letter)? @display.correct(letter, @brain.word) : @display.error_step(letter)
+		end
+  end
+
   def pick_difficulty
-		#system 'clear'
+    #system 'clear'
     puts 'Choose your difficulty'
     puts 'EASY (5 - 6 Characters) NORMAL (7 - 9 Characters) or HARD (10 - 12 Characters)'
     @difficulty = gets.chomp.downcase until @difficulties.include?(@difficulty)
